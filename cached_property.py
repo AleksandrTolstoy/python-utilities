fd92ce57aa54968e5025b99621f21af5b1646666
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-05 15:20:46 +0300

    implemented a cached property

---
+++ cached_property.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+class cached_property:
+    """Allows you to calculate attributes directly when they are accessed
+
+    Useful for offloading object initialization
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls=None):
+        result = instance.__dict__[self.func.__name__] = self.func(instance)
+        return result
