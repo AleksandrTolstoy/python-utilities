987ff0652ec2d8d3db346fc406f8e30ce2e28104
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-05 16:25:57 +0300

    implemented <logged>

---
+++ logged.py
@@ -0,0 +1,34 @@
+import time
+from functools import wraps
+
+
+def logged(time_format='%b %d %Y - %H:%M:%S', separator=''):
+    def decorator(func):
+        @wraps(func)
+        def decorated_func(*args, **kwargs):
+            arg_lst = []
+            if args:
+                arg_lst.append(', '.join(repr(arg) for arg in args))
+            if kwargs:
+                pairs = [f'{k}={w}' for k, w in sorted(kwargs.items())]
+                arg_lst.append(', '.join(pairs))
+            arg_str = ', '.join(arg_lst)
+
+            print(f'{separator}- Running {func.__qualname__}({arg_str}) on {time.strftime(time_format)}')
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f'- Finished {func.__qualname__}({arg_str}), execution time = {end_time - start_time}s{separator}')
+            return result
+        return decorated_func
+    return decorator
+
+if __name__ == '__main__':
+    @logged(separator='\n')
+    def func(a, b, c, d=1, e=2):
+        print('in func')
+
+    func(1, 2, 3, **{'d':4, 'e':5})
+    func(5, 4, 3)
+
+
