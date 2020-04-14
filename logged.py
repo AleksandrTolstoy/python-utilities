bb745007162d8b49ea7ad701ae4336a24c385000
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-04-14 22:58:48 +0300

    Added coloring and modes

--- logged.py
+++ logged.py
@@ -2,35 +2,57 @@
 # -*- coding: UTF-8 -*-
 
 import time
+from enum import Enum, auto
 from functools import wraps
+from typing import Any, Tuple, Dict
+
+from termcolor import colored
 
 
-def logged(time_format='%b %d %Y - %H:%M:%S', separator=''):
+class Modes(Enum):
+    WITH_PARAMETERS: int = auto()
+    WITHOUT_PARAMETERS: int = auto()
+
+
+def args_builder(mode: Modes, /, args: Tuple[Any, ...], kwargs: Dict[str, Any]) -> str:
+    if mode is Modes.WITH_PARAMETERS:
+        arg_lst = []
+        if args:
+            arg_lst.append(colored(', '.join(repr(arg) for arg in args), 'green'))
+        if kwargs:
+            pairs = [f'{k}={w}' for k, w in sorted(kwargs.items())]
+            arg_lst.append(colored(', '.join(pairs), 'cyan'))
+
+        arg_str = ', '.join(arg_lst)
+        return colored(f'({arg_str})')
+    elif mode is Modes.WITHOUT_PARAMETERS:
+        return ''
+
+
+def logged(time_format='%b %d %Y - %H:%M:%S', separator='', mode: Modes = Modes.WITHOUT_PARAMETERS):
     def decorator(func):
         @wraps(func)
         def decorated_func(*args, **kwargs):
-            arg_lst = []
-            if args:
-                arg_lst.append(', '.join(repr(arg) for arg in args))
-            if kwargs:
-                pairs = [f'{k}={w}' for k, w in sorted(kwargs.items())]
-                arg_lst.append(', '.join(pairs))
-            arg_str = ', '.join(arg_lst)
+            arg_str = args_builder(mode, args, kwargs)
+            func_disp = colored(func.__qualname__, "magenta", attrs=["bold"])
+            print(f'{separator}- Running {func_disp}'
+                  f'{arg_str} on {time.strftime(time_format)}')
 
-            print(f'{separator}- Running {func.__qualname__}({arg_str}) on {time.strftime(time_format)}')
             start_time = time.time()
             result = func(*args, **kwargs)
-            print(f'- Finished {func.__qualname__}({arg_str}), execution time = {time.time() - start_time}s{separator}')
+            print(f'- Finished {func_disp}'
+                  f'{arg_str}, execution time = {time.time() - start_time}s{separator}')
             return result
+
         return decorated_func
+
     return decorator
 
+
 if __name__ == '__main__':
-    @logged(separator='\n')
+    @logged(separator='\n', mode=Modes.WITH_PARAMETERS)
     def func(a, b, c, d=1, e=2):
         print('in func')
 
-    func(1, 2, 3, **{'d':4, 'e':5})
+    func(1, 2, 3, **{'d': 4, 'e': 5})
     func(5, 4, 3)
-
-
